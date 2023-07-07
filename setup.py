91b0c48c9dd63d094a2019a2b49b70fdfa504a71
Yorick Chern <yorio@yoricks-mbp.lan>
2023-07-06 23:23:42 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='matplotlib_ai',
+      version='1.0',
+      description='A GPT-powered tool to bring no-code data visualization to life!',
+      author='Yorick Chern',
+      author_email='yorichek.007@gmail.com',
+      url='https://github.com/notY0rick/mpl_ai',
+      packages=['openai', 'matplotlib'],
+      keywords=['python', 'gpt', 'matplotlib', 'no code', 'LLM'],
+      license='MIT'
+     )
